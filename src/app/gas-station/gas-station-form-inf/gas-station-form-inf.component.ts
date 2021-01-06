b1c7cebe4f5da9ec005fd0ed7f73fa1fc213a3ea
Luiz Fernando <luizfernandorole@gmail.com>
2021-01-06 13:58:47 -0300

    Botão de compartilhamento

--- src/app/gas-station/gas-station-form-inf/gas-station-form-inf.component.ts
+++ src/app/gas-station/gas-station-form-inf/gas-station-form-inf.component.ts
@@ -17,4 +17,18 @@ export class GasStationFormInfComponent implements OnInit {
   redirectToMaps(latitude: number, longitude: number): void {
     window.open(`https://www.google.com.br/maps/search/${latitude},+${longitude}`);
   }
+
+  share(latitude: string, longitude: string): void {
+    if (!('share' in navigator)) {
+      alert('Share API not supported.');
+      return;
+    }
+
+    navigator.share({
+      title: 'Economize dinheiro!!',
+      text: 'O preço mais barato para abastecer seu carro você encontra aqui!',
+      url: `https://www.google.com.br/maps/search/${latitude},+${longitude}`
+    })
+      .catch(error => alert('Error sharing: ' + error));
+  }
 }
