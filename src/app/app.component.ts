c25661d80547bd09094d29453423034123f473d2
Luiz Fernando <luizfernandorole@gmail.com>
2021-01-08 00:36:55 -0300

    Tentativa de implementar OG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+import {titleMainPage, descriptionMainPage} from './core/utils';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Hash Fuel';
+export class AppComponent implements OnInit{
+  constructor(
+    private titleService: Title,
+    private metaService: Meta
+  ) {}
+  ngOnInit(): void {
+    const baseUrl = window.location.protocol + '//' + window.location.hostname;
+    const imageUrl = baseUrl + '/fuel.png';
+    this.titleService.setTitle(titleMainPage);
+    this.metaService.addTags([
+      {property: 'og:description', content: descriptionMainPage},
+      {property: 'og:image', content: imageUrl, itemprop: 'image'},
+      {property: 'og:url', content: baseUrl},
+      {property: 'og:site_name', content: titleMainPage},
+      {property: 'og:type', content: 'website'},
+      {property: 'og:updated_time', content: String(Date.now())},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+    ]);
+  }
 }
--- src/app/core/utils.ts
+++ src/app/core/utils.ts
@@ -1,1 +1,4 @@
 export const mainTitle = 'Know Gas';
+export const titleMainPage = 'Know Gas | Saiba o preço mais barato para abastecer seu carro!';
+export const descriptionMainPage = 'Feito para você conseguir economizar uma graninha extra, podendo abastecer seu carro nos postos com o melhor preço de Aguas Claras!!';
+
