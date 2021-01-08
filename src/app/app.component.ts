7efca1f1c5b022757aefee9c7f5f8b29c97499cf
Luiz Fernando <luizfernandorole@gmail.com>
2021-01-08 12:58:43 -0300

    Aplicando og

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import { Title, Meta } from '@angular/platform-browser';
-import {titleMainPage, descriptionMainPage} from './core/utils';
+import {Component} from '@angular/core';
 
 
 @Component({
@@ -8,23 +6,6 @@ import {titleMainPage, descriptionMainPage} from './core/utils';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  constructor(
-    private titleService: Title,
-    private metaService: Meta
-  ) {}
-  ngOnInit(): void {
-    const baseUrl = window.location.protocol + '//' + window.location.hostname;
-    const imageUrl = baseUrl + '/fuel.png';
-    this.titleService.setTitle(titleMainPage);
-    this.metaService.addTags([
-      {property: 'og:description', content: descriptionMainPage},
-      {property: 'og:image', content: imageUrl, itemprop: 'image'},
-      {property: 'og:url', content: baseUrl},
-      {property: 'og:site_name', content: titleMainPage},
-      {property: 'og:type', content: 'website'},
-      {property: 'og:updated_time', content: String(Date.now())},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-    ]);
-  }
+export class AppComponent {
+
 }
